254f68b04ee36c25e6c9d599d10479fb9b56d691
André Masson <amwebexpert@gmail.com>
2019-08-18 17:35:09 -0400

    Added Github example

--- simple/src/main/kotlin/com/example/simple/SecurityConfiguration.kt
+++ simple/src/main/kotlin/com/example/simple/SecurityConfiguration.kt
@@ -18,7 +18,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import org.springframework.web.filter.CompositeFilter
+import java.util.*
 import javax.servlet.Filter
+
 
 @Configuration
 @EnableWebSecurity
@@ -35,21 +38,37 @@ class SecurityConfiguration(val oauth2ClientContext: OAuth2ClientContext) : WebS
                 .and()
                 .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
-                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter::class.java)
+                .addFilterBefore(ssoCompositeFilter(), BasicAuthenticationFilter::class.java)
         // @formatter:on
     }
 
-    private fun ssoFilter(): Filter {
+    private fun ssoCompositeFilter(): Filter {
+        val filter = CompositeFilter()
+        val filters = ArrayList<Filter>()
+
         val facebookFilter = OAuth2ClientAuthenticationProcessingFilter("/login/facebook")
         val facebookTemplate = OAuth2RestTemplate(facebook(), oauth2ClientContext)
         facebookFilter.setRestTemplate(facebookTemplate)
-        val tokenServices = UserInfoTokenServices(facebookResource().userInfoUri, facebook().clientId)
+        var tokenServices = UserInfoTokenServices(facebookResource().userInfoUri, facebook().clientId)
         tokenServices.setRestTemplate(facebookTemplate)
         facebookFilter.setTokenServices(tokenServices)
-        return facebookFilter
+        filters.add(facebookFilter)
+
+        val githubFilter = OAuth2ClientAuthenticationProcessingFilter("/login/github")
+        val githubTemplate = OAuth2RestTemplate(github(), oauth2ClientContext)
+        githubFilter.setRestTemplate(githubTemplate)
+        tokenServices = UserInfoTokenServices(githubResource().getUserInfoUri(), github().getClientId())
+        tokenServices.setRestTemplate(githubTemplate)
+        githubFilter.setTokenServices(tokenServices)
+        filters.add(githubFilter)
+
+        filter.setFilters(filters)
+        return filter
+
     }
 
     @Bean
+
     fun oauth2ClientFilterRegistration(filter: OAuth2ClientContextFilter): FilterRegistrationBean<OAuth2ClientContextFilter> {
         val registration = FilterRegistrationBean<OAuth2ClientContextFilter>()
         registration.filter = filter
@@ -66,6 +85,18 @@ class SecurityConfiguration(val oauth2ClientContext: OAuth2ClientContext) : WebS
     @Bean
     @ConfigurationProperties("facebook.resource")
     fun facebookResource(): ResourceServerProperties {
+        return ResourceServerProperties()
+    }
+
+    @Bean
+    @ConfigurationProperties("github.client")
+    fun github(): AuthorizationCodeResourceDetails {
+        return AuthorizationCodeResourceDetails()
+    }
+
+    @Bean
+    @ConfigurationProperties("github.resource")
+    fun githubResource(): ResourceServerProperties {
         return ResourceServerProperties()
     }
 
